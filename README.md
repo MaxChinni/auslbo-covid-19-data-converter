54b29f7b5bcb619587b5f754d18c606273f9833c
Massimiliano Chinni <max@mx17.net>
2020-03-30 01:11:36 +0200

    New features
      * added more data files
      * script now works on multiple input files
      * added README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# N.B.
+Converter seems to work on data from 2020-03-26
+
+## Usage
+
+    ./converter.sh data/Report\ casi\ e\ sorveglianza\ AUSL\ BO\ 2[6-9]* | fzf
--- converter.sh
+++ converter.sh
@@ -8,58 +8,63 @@ extractDateFromFilename()
     date +'%Y-%m-%d' -d "$extrDate"
 }
 
-extractDateFromFilename "$1"
-
-pdftotext "$1" - | \
-    tr -d '' | \
-    awk '
-    function printData()
-    {
-        if (length(labels) == 0) {
-            return
-        }
-        printf "\n# %s\n\n", title
-        printf "| %-30s | %3s |\n", labelName, " n "
-        printf "+--------------------------------+-----+\n", labelName, ""
-        for (h in labels) {
-            printf "| %-30s | %3d |\n", labels[h], values[h]
-        }
-    }
-    {
-        #print "DEBUG", "SECTION", section, "LINE:", $0, "LENGTH", length($0)
-        if ($0 ~ /^[[:space:]]*$/) {
-            # empty line
-            if (section == "values" && c > 0) {
-                printData()
-                split("", labels, ":")
+parseFile()
+{
+    pdftotext "$1" - | \
+        tr -d '' | \
+        awk -v theDate="$theDate" '
+        function printData()
+        {
+            if (length(labels) == 0) {
+                return
             }
-        } else if ($0 ~ /^Numero /) {
-            title = $0
-        } else if ($0 == "Distretto") {
-            if (prevLine != "") {
+            for (h in labels) {
+                printf "| %s | %-80s | %-30s | %3d |\n", theDate, title, labels[h], values[h]
+            }
+        }
+        {
+            #print "DEBUG", "SECTION", section, "LINE:", $0, "LENGTH", length($0)
+            if ($0 ~ /^[[:space:]]*$/) {
+                # empty line
+                if (section == "values" && c > 0) {
+                    printData()
+                    split("", labels, ":")
+                }
+            } else if ($0 ~ /^Numero /) {
+                title = $0
+                gsub("/^ *(.*) *$/", "xx \1", title)
+            } else if ($0 == "Distretto") {
+                if (prevLine != "") {
+                    title = prevLine
+                    gsub("/^ *(.*) *$/", "xx \1", title)
+                }
+                section = $0
+                labelName = $0
+                c = 0
+            } else if ($0 == "Comune") {
                 title = prevLine
+                gsub("/^ *(.*) *$/", "xx \1", title)
+                section = $0
+                labelName = $0
+                c = 0
+            } else if ($0 == "Totale") {
+                section = $0
+            } else if ($0 == "∆" || $0 == "n") {
+                # end section "Distretto"
+                section = "values"
+                split("", values, ":")
+                c = 0
+            } else if (section == "Distretto" || section == "Comune") {
+                labels[++c] = $0
+            } else if (section == "values" && $0 !~ /^\+/) {
+                values[++c] = $0
             }
-            section = $0
-            labelName = $0
-            c = 0
-        } else if ($0 == "Comune") {
-            title = prevLine
-            section = $0
-            labelName = $0
-            c = 0
-        } else if ($0 == "Totale") {
-            section = $0
-        } else if ($0 == "∆" || $0 == "n") {
-            # end section "Distretto"
-            section = "values"
-            split("", values, ":")
-            c = 0
-        } else if (section == "Distretto" || section == "Comune") {
-            labels[++c] = $0
-        } else if (section == "values" && $0 !~ /^\+/) {
-            values[++c] = $0
-        }
 
-        prevLine = $0
+            prevLine = $0
+        }'
+}
 
-    }'
+for f in "$@"; do
+    theDate=$(extractDateFromFilename "$f")
+    parseFile "$f"
+done
